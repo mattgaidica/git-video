acdbdda9889999c7a8a44f3a0bccbe819b62730e
Matt <matt@primestudiosllc.com>
2013-01-22 23:17:33 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitVideo::Application.config.secret_token = '9a199cf125bf105d36635c13b9510933b559a76e8df274b6cbdbdcc20c83da385e667078ba6fef6649d4416a7680578814afb02420d3d0ca159c239494959290'
